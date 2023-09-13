c34e48c15050371355fbdb49f0196339e1d02549
megahomyak <g.megahomyak@gmail.com>
2023-09-14 01:24:13 +0500

    Made it delete dirs normally

--- cleanpy.sh
+++ cleanpy.sh
@@ -2,7 +2,7 @@
 
 shopt -s extglob
 this=$(basename "$0")
-find . -mindepth 1 ! -name "$this" -exec rm -rf {} \;
+find . -mindepth 1 -maxdepth 1 ! -name "$this" -exec rm -rf {} \;
 yes '' | poetry init
 printf "\n\n=== RESULTS: ===\n\n"
 ls
