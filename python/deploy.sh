25518f1f6adf8e1c8da5cdfe63995a41764fc394
megahomyak <g.megahomyak@gmail.com>
2023-10-05 16:03:09 +0500

    `--remove-untracked` -> `--sync`

--- python/deploy.sh
+++ python/deploy.sh
@@ -3,6 +3,6 @@
 sshpass -p <password> ssh <user>@<ip> << EOF
     cd <dir>
     GIT_SSH_COMMAND='ssh -i <deploy key path> -o IdentitiesOnly=yes' git pull
-    poetry install --no-root --remove-untracked
+    poetry install --no-root --sync
     systemctl restart <service name>
 EOF
