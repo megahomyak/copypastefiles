82cde8246c62bff1d10a5984964e5d8f6b90d6b1
megahomyak <g.megahomyak@gmail.com>
2023-10-05 12:20:10 +0500

    Added some more templates

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+sshpass -p <password> ssh <user>@<ip> << EOF
+    cd <dir>
+    GIT_SSH_COMMAND='ssh -i <deploy key path> -o IdentitiesOnly=yes' git pull
+    poetry install --no-root
+    systemctl restart <service name>
+EOF
